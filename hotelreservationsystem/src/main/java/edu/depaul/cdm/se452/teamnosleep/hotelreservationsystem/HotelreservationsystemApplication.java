990693d5acec3bf0d9e6edb702d337cfdd617037
RiddhiDamani <riddhidamani90@gmail.com>
2021-04-10 10:00:31 -0500

    Milestone1 Skeleton Code

---
+++ hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/HotelreservationsystemApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelreservationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelreservationsystemApplication.class, args);
+	}
+
+}
---
+++ hotelreservationsystem/src/test/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/HotelreservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelreservationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
