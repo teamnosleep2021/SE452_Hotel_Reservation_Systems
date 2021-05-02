e5526c673edde94d30765ebea650b74ff0a8cf31
Jared Schreiber <mv3br8gsk3@protonmail.com>
2021-05-02 09:25:55 -0500

    updates

--- hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/HotelreservationsystemApplication.java
+++ hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/HotelreservationsystemApplication.java
@@ -3,15 +3,12 @@ package edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
 
 import edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem.repositories.UsersRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
->>>>>>> upstream/main
 
 @SpringBootApplication
 public class HotelreservationsystemApplication {
@@ -21,9 +18,10 @@ public class HotelreservationsystemApplication {
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(HotelreservationsystemApplication.class);
-/*
+
+    //search functionality
 	@Bean
-    public CommandLineRunner demo(UsersRepository repository) {
+    public CommandLineRunner find(UsersRepository repository) {
         return (args) -> {
             // fetch all customers
             log.info("Users found with findAll():");
@@ -34,5 +32,19 @@ public class HotelreservationsystemApplication {
             log.info("-------------------------------");
         };
     }
-*/
+
+    //create functionality
+	@Bean
+    public CommandLineRunner create(UsersRepository repository) {
+        return (args) -> {
+            // fetch all customers
+            log.info("Users found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().forEach((User) -> {
+                log.info(User.toString());
+            });
+            log.info("-------------------------------");
+        };
+    }
+
 }
