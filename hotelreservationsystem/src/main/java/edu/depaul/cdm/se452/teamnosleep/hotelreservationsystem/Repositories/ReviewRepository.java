0818220547bd9a31c369527997b0c15ee6a6eb79
Jared Schreiber <mv3br8gsk3@protonmail.com>
2021-05-14 23:28:32 -0500

    removing older folders

--- hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/Repositories/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import edu.depaul.cdm.se452.teamnosleep.hotelreservationsystem.entities.Rating;
-
-public interface ReviewRepository extends MongoRepository<Rating, String> {
-    
-}
