a33248cfcc79a24ded0235fb1269b2cf1813d203
Jared Schreiber <mv3br8gsk3@protonmail.com>
2021-05-16 12:54:49 -0500

    adding html for reservations

--- hotelreservationsystem/src/main/resources/data-postgresql.sql
+++ hotelreservationsystem/src/main/resources/data-postgresql.sql
@@ -34,29 +34,29 @@ insert into room_types(TYPE_STR) values('Double Room');
 insert into room_types(TYPE_STR) values('Queen Room');
 
 -- populate rooms
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 1 , 2, 250);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 1 , 4, 500);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 1 , 6, 800);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 2 , 2, 300);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 2 , 4, 600);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 2 , 6, 1000);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 3 , 2, 150);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 3 , 4, 250);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 4 , 6, 400);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 5 , 2, 250);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 5 , 4, 500);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 5 , 6, 800);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 6 , 2, 650);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 6 , 4, 750);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 6 , 6, 1200);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 7 , 2, 200);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 7 , 4, 400);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 7 , 6, 600);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 8 , 2, 300);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 8 , 4, 350);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (3, 8 , 6, 650);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (1, 9 , 2, 150);
-insert into rooms(ROOM_TYPE, hotel_id, guests, COST_PER_NIGHT) values (2, 10 , 4, 400);
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 1 , 2, 250,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 1 , 4, 500,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 1 , 6, 800,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 2 , 2, 300,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 2 , 4, 600,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 2 , 6, 1000,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 3 , 2, 150,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 3 , 4, 250,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 4 , 6, 400,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 5 , 2, 250,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 5 , 4, 500,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 5 , 6, 800,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 6 , 2, 650,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 6 , 4, 750,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 6 , 6, 1200,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 7 , 2, 200,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 7 , 4, 400,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 7 , 6, 600,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 8 , 2, 300,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 8 , 4, 350,'R2');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (3, 8 , 6, 650,'R3');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (1, 9 , 2, 150,'R1');
+insert into rooms(ROOM_TYPE_ID, hotel_id, guests, COST_PER_NIGHT,room_name) values (2, 10 , 4, 400,'R1');
 
 
 -- populate reservations
