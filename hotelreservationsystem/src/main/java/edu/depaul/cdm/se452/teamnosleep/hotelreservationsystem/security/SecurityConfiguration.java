e02d29d0e170db7c5c0258121c141dbcd2c8b0c0
RiddhiDamani <riddhidamani90@gmail.com>
2021-05-29 11:22:24 -0500

    1. Fixed UI Changes 2. Added security permissions for guests to view the hotel rooms, but not to book.

--- hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/security/SecurityConfiguration.java
+++ hotelreservationsystem/src/main/java/edu/depaul/cdm/se452/teamnosleep/hotelreservationsystem/security/SecurityConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -45,8 +47,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/").permitAll()
             .antMatchers("/register").permitAll()
             .antMatchers("/process-register").permitAll()
-            //.antMatchers("/hotel-details.html?**").permitAll()
-            //.antMatchers("/").hasAnyAuthority("ADMIN","USER")
+            .antMatchers("/hotel-details**").permitAll()
+            .antMatchers("/book-room**").permitAll()
             .antMatchers("/cancellation.html").hasAuthority("ADMIN")
             .antMatchers("/updated.html").hasAnyAuthority("ADMIN", "USER")
             .anyRequest().authenticated()
@@ -58,8 +60,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login")
                 .permitAll()
             .and()
-            .logout().permitAll()
+            .logout()
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login?logout")
+            .permitAll()
             .and()
             .exceptionHandling().accessDeniedPage("/access-denied");
+        
+        // disabling csrf attack 
+        http.csrf().disable(); 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/images/**");
+        web.ignoring().antMatchers("/js/**");
     }
 }
